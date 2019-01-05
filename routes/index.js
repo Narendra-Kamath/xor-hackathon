53f8ba007a3aa5f27a3fe57647c25d69745e2da9
Navaneeth Mohan <navaneeth.mohan@rybbon.net>
2019-01-05 14:48:46 +0530

    Updated Routes

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,10 @@ const express = require('express')
 
 const router = express.Router()
 
-const smapleEndPoint = require('./sample-controller')
 const medicineController = require ('../../controller/MedicineController')
 const ingredientsController = require ('../../controller/IngredientsController')
 
-router.get('/add', smapleEndPoint.add)
 router.post('/searchMedicine', medicineController.searchMedicine)
 router.post('/getIngredients', ingredientsController.getIngreditentsForMedicine)
 
-module.exports = router
+module.exports = routadd
