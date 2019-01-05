446136a746ccfef8de5edcc2a37ff2fa713eb41c
Navaneeth Mohan <navaneeth.mohan@rybbon.net>
2019-01-05 14:59:17 +0530

    Fix

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router()
 const medicineController = require ('../controller/MedicineController')
 const ingredientsController = require ('../controller/IngredientsController')
 
-router.post('/searchMedicine', medicineController.searchMedicine)
+router.get('/searchMedicine', medicineController.searchMedicine)
 router.post('/getIngredients', ingredientsController.getIngreditentsForMedicine)
 
 module.exports = router
